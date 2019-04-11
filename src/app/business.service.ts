52654cdd346a457d3fd05b405a01bc454c00b534
Guest User <Guest@epicodus-31.local>
2019-04-11 13:43:23 -0700

    continue working business service

--- src/app/business.service.ts
+++ src/app/business.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Business } from "./business.model";
+import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 
 @Injectable()
 export class BusinessService {
+  businesses: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.businesses = database.list("businesses");
+  }
 
   getBusinesses() {
   return this.businesses;
