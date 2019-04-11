ef557e0873ebe467dd89fea5e3835e55fb724c18
Guest User <Guest@epicodus-31.local>
2019-04-11 14:20:00 -0700

    add edit business

--- src/app/edit-business/edit-business.component.ts
+++ src/app/edit-business/edit-business.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Business } from '../business.model';
+import { BusinessService } from '../business.service';
 
 @Component({
   selector: 'app-edit-business',
   templateUrl: './edit-business.component.html',
-  styleUrls: ['./edit-business.component.css']
+  styleUrls: ['./edit-business.component.css'],
+  providers: [BusinessService]
 })
 export class EditBusinessComponent implements OnInit {
+  @Input() selectedBusiness;
 
-  constructor() { }
+  constructor(private businessService: BusinessService) { }
 
   ngOnInit() {
   }
+
+  beginUpdatingBusiness(businessToUpdate){
+  this.businessService.updateBusiness(businessToUpdate);
+}
+
+beginDeletingBusiness(businessToDelete){
+  if(confirm(`Are you sure you want to delete ${gameToDelete.title}?`)){
+    this.businessService.deleteBusiness(businessToDelete);
+  }
+}
 
 }
