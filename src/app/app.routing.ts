3f0aa66eb5bb8fc945a78da82c095d3bfbf4abd6
Guest User <Guest@epicodus-31.local>
2019-04-11 11:29:33 -0700

    add routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { AboutComponent }   from './about/about.component';
 import { HollerComponent }   from './holler/holler.component';
+import { BusinessDetailComponent }from "./business-detail/business-detail.component";
 
 const appRoutes: Routes = [
   {
@@ -16,6 +17,10 @@ const appRoutes: Routes = [
   {
     path: 'holler',
     component: HollerComponent
+  },
+  {
+    path: 'businesses/:id',
+    component: BusinessDetailComponent
   }
 ];
 
