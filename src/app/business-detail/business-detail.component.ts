4b4884f0fecd37a32f92bc8b900d39f28128290b
Guest User <Guest@epicodus-31.local>
2019-04-11 12:57:59 -0700

    continue working on business detail component

--- src/app/business-detail/business-detail.component.ts
+++ src/app/business-detail/business-detail.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Business } from '../business.model';
 
 @Component({
   selector: 'app-business-detail',
   templateUrl: './business-detail.component.html',
   styleUrls: ['./business-detail.component.css']
 })
-export class BusinessDetailComponent implements OnInit {
+  export class BusinessDetailComponent implements OnInit {
+    businessId: string;
+    businessToDisplay;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location,private businessService: BusinessService) { }
 
   ngOnInit() {
-  }
+    this.route.params.forEach((urlParameters) => {
+    this.businessId = urlParameters['id'];
+    });
+    this.businessService.getBusinessById(this.businessId).subscribe(dataLastEmittedFromObserver => {
+    this.businessToDisplay = new Business(dataLastEmittedFromObserver.title, dataLastEmittedFromObserver.review, dataLastEmittedFromObserver.description);
+    });
 
+  }
 }
--- src/app/holler/holler.component.ts
+++ src/app/holler/holler.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Business } from '../business.model';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -8,6 +9,11 @@ import { Business } from '../business.model';
   styleUrls: ['./holler.component.css']
 })
 export class HollerComponent implements OnInit {
+  currentRoute: string = this.router.url;
+
+  goToDetailPage(clickedBusiness){
+  this.router.navigate(["businesses", clickedBusiness.$key]);
+  }
 
   constructor() { }
 
